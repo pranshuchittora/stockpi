8cde9c8f37b25125e88be3c522ab74181fd1d70c
Pranshu Chittora <pranshuchittora17@gmail.com>
2020-07-02 19:08:35 +0530

    chore: Added proper comments and minor logic changes

--- main.py
+++ main.py
@@ -11,17 +11,22 @@ load_dotenv()
 LED_BRIGHTNESS = 0.2
 LED_COLOR_PEAK = 10
 DISPLAY_SPEED = 0.3
-STOCK_LIST = ["GOOG", "INTC", "MSFT", "FB", "PYPL", "AAPL"]
+STOCK_LIST = ["GOOG", "INTC", "MSFT", "AMZN", "FB", "PYPL", "AAPL"]
 STOCK_POINTER = 0
 
+# Setup of the Finnhub
 configuration = finnhub.Configuration(
     api_key={
         'token': os.getenv("FINNHUB_TOKEN")
     })
 
+# Finnhub client
 finnhub_client = finnhub.DefaultApi(finnhub.ApiClient(configuration))
 
 
+# Finds the postion of the "." in the given index
+#  @params str: string
+# @returns String[]
 def findDotsInString(str):
     dotLocation = []
     i = 0
@@ -32,12 +37,17 @@ def findDotsInString(str):
     return dotLocation
 
 
+# Resets all the "." in the 16 segment display
+# @retruns void
 def resetAlldots():
     for id in range(0, 4):
         rh.display.set_decimal(id, False)
         rh.display.show()
 
 
+# Calculates the stock's trends i.e. going up or going down
+# @params current_price : number, previous_close: number
+# @returns string "+" or "-"
 def stockTrend(current_price, previous_close):
     stock_trend = "+"
     if current_price < previous_close:
@@ -45,7 +55,10 @@ def stockTrend(current_price, previous_close):
     return stock_trend
 
 
-def showLedBasedOnPercentage(percentage):
+# LEDs to light up based on the percentage and stock's trend
+# @params percentage: number, stock_trend: string ("+" | "-")
+# @returns void
+def showLedBasedOnPercentage(percentage, stock_trend):
     led_numbers = percentage
 
     if led_numbers > 7:
@@ -67,7 +80,7 @@ def showLedBasedOnPercentage(percentage):
                                  brightness=LED_BRIGHTNESS)
     rh.rainbow.show()
 
-
+# Increments the stock pointer to the next stock in the array (index)
 def incrementStockPointer():
     global STOCK_POINTER
     STOCK_POINTER += 1
@@ -81,15 +94,18 @@ while True:
         stockData = finnhub_client.quote(stockName)
         previous_close = stockData.pc
         current_price = stockData.c
+        # Stock's trend
         stock_trend = stockTrend(current_price, previous_close)
+        # % change
         percentage = ((current_price - previous_close) / previous_close) * 100
-
         percentage = abs(float("{:.2f}".format(percentage)))
+        
         display_string = "   "+stockName + " " + \
             str(current_price) + " " + stock_trend + str(percentage) + "%     "
         dotPosition = findDotsInString(display_string)
 
-        showLedBasedOnPercentage(percentage)
+        showLedBasedOnPercentage(percentage, stock_trend)
+        
         print(display_string)
 
         display_string = display_string.replace(".", "")
