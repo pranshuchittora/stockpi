06fb7f2d7b51f323bd7c0ef6b228eb93d49522f5
Pranshu Chittora <pranshuchittora17@gmail.com>
2020-07-02 06:29:31 +0530

    feat: Added docs and code

---
+++ README.md
@@ -0,0 +1,107 @@
+<div style="text-align:center"><img alt="StockPi Logo" src="./docs/logo.png" /></div>
+
+StockPi is a RaspberryPi based real-time stock price monitoring system powered by [Finnhub's API](https://finnhub.io/)
+
+## Motivation
+
+I am a huge stock market enthusiast and monitoring stock prices in realtime requires a dedicated device. So I decided to make it fun and intuitive by using a Raspberry Pi rusting in my cupboard :)
+
+## Getting up and running
+
+### Hardware
+
+- RaspbeeryPi 3 or above.
+- [Rainbow HAT](https://shop.pimoroni.com/products/rainbow-hat-for-android-things)
+- Micro USB adapter (Old mobile phone charger).
+
+### Software
+
+- RasbianOS or something similar
+- Python3
+- [rainboaw-hat (Python)](https://github.com/pimoroni/rainbow-hat)
+- Finnhub API Token
+
+---
+
+### Let's get this started
+
+Clone the repo
+
+```
+git clone https://github.com/pranshuchittora/stockpi.git
+```
+
+Install the dependencies
+
+```bash
+ pip3 install -r requirements.txt
+```
+
+Setup Finnhub account and generate API token. Copy and paste the token in the `.env` file.
+
+```.env
+FINNHUB_TOKEN='FINNHUB_API_TOKEN'
+```
+
+Run `main.py`
+
+```bash
+python3 main.py
+```
+
+**Voilla 🤗**
+
+## Service setup on system startup (MacOS/Linux only)
+
+Main advantages of running this on Raspberry Pi is that it's a low power device and we can configure it to auto-run the script on startup which makes it hassle-free.
+
+Change file permissions
+
+```bash
+chmod u+x main.py
+```
+
+### Create a system service
+
+Run the following command to create a service
+
+```
+> sudo systemctl edit --force --full stockpi.service
+```
+
+Insert these statements with your settings, save them and quit the editor
+
+```
+[Unit]
+Description=StockPi Service
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User=pi
+WorkingDirectory=/home/pi/stockpi
+ExecStart=/home/pi/stockpi/main.py
+
+[Install]
+WantedBy=multi-user.target
+```
+
+Run `systemctl status stockpi.service` to get the service status
+
+```
+> systemctl status stockpi.service
+
+● stockpi.service - StockPi Service
+    Loaded: loaded (/etc/systemd/system/stockpi.service; disabled; vendor preset: enabled)
+    Active: inactive (dead)
+```
+
+Now let's enable and start the service
+
+```
+> sudo systemctl enable stockpi.service
+> sudo systemctl start stockpi.service
+```
+
+Yaaay, now to don't need to start the program manually. I'm not a robot 🤖
---
+++ main.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+
+import finnhub
+import rainbowhat as rh
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LED_BRIGHTNESS = 0.2
+LED_COLOR_PEAK = 10
+DISPLAY_SPEED = 0.3
+STOCK_LIST = ["GOOG", "ACCO", "MSFT", "FB", "PYPL", "AAPL"]
+STOCK_POINTER = 0
+
+configuration = finnhub.Configuration(
+    api_key={
+        'token': os.getenv("FINNHUB_TOKEN")
+    })
+    
+finnhub_client = finnhub.DefaultApi(finnhub.ApiClient(configuration))
+
+
+def findDotsInString(str):
+    dotLocation = []
+    i = 0
+    for idx in range(len(str)):
+        if str[idx] == '.':
+            dotLocation.append(idx - 1 - i)
+            i += 1
+    return dotLocation
+
+
+def resetAlldots():
+    for id in range(0, 4):
+        rh.display.set_decimal(id, False)
+        rh.display.show()
+
+
+def stockTrend(current_price, previous_close):
+    stock_trend = "+"
+    if current_price < previous_close:
+        stock_trend = "-"
+    return stock_trend
+
+
+def showLedBasedOnPercentage(percentage):
+    led_numbers = percentage
+
+    if led_numbers > 7:
+        led_numbers = 7
+    rh.rainbow.clear()
+
+    for x in range(int(led_numbers)):
+        if stock_trend == "+":
+            rh.rainbow.set_pixel(6 - x,
+                                 0,
+                                 LED_COLOR_PEAK,
+                                 0,
+                                 brightness=LED_BRIGHTNESS)
+        else:
+            rh.rainbow.set_pixel(6 - x,
+                                 LED_COLOR_PEAK,
+                                 0,
+                                 0,
+                                 brightness=LED_BRIGHTNESS)
+    rh.rainbow.show()
+
+
+while True:
+    try:
+        stockName = STOCK_LIST[STOCK_POINTER]
+        # Fetches Stock Data
+        stockData = finnhub_client.quote(stockName)
+        previous_close = stockData.pc
+        current_price = stockData.c
+        stock_trend = stockTrend(current_price, previous_close)
+        percentage = ((current_price - previous_close) / previous_close) * 100
+
+        percentage = abs(float("{:.2f}".format(percentage)))
+        display_string = "   "+stockName + " " + \
+            str(current_price) + " " + stock_trend + str(percentage) + "%     "
+        dotPosition = findDotsInString(display_string)
+
+        showLedBasedOnPercentage(percentage)
+        print(display_string)
+
+        display_string = display_string.replace(".", "")
+
+        for element in range(0, (len(display_string) - 4)):
+            rh.display.print_str(display_string[element] +
+                                 display_string[element + 1] +
+                                 display_string[element + 2] +
+                                 display_string[element + 3])
+            rh.display.show()
+            resetAlldots()
+
+            for i in range(0, 4):
+                if (element + i in dotPosition):
+                    rh.display.set_decimal(i, True)
+
+            rh.display.show()
+            time.sleep(DISPLAY_SPEED)
+
+        STOCK_POINTER += 1
+        STOCK_POINTER = STOCK_POINTER % len(STOCK_LIST)
+    except:
+        rh.display.print_str("ERRR")
+        rh.display.show()
+        rh.rainbow.set_all(LED_COLOR_PEAK, 0, 0, brightness=LED_BRIGHTNESS)
+        rh.rainbow.show()
+        time.sleep(3)
